641888fd0c1e220385b98007571ceb5cd3ce52b8
Adarsh <git@lti>
2020-02-17 18:44:42 +0530

    dao changes

--- Banking/src/com/lti/banking/core/daos/BankDao.java
+++ Banking/src/com/lti/banking/core/daos/BankDao.java
@@ -4,5 +4,6 @@ import com.lti.banking.core.entities.Register;
 import com.lti.banking.core.exceptions.HrException;
 
 public interface BankDao {
-	public boolean insertNewUser(Register reg) throws HrException;
+	
+	public boolean insertNewUser(Register reg) throws HrException; 
 }
